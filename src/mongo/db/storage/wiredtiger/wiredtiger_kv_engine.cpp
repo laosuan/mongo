58bae9f18f0dff2eae0e4e16a1a0bf7022db8395
Matt Kneiser <matt.kneiser@mongodb.com>
2026-02-16 21:30:55 -0800

    SERVER-119520 Expose fastcount flush behavior to WTKVEngine (#48151)
    GitOrigin-RevId: c8758fd1ac96432408a0b6522d9e36988b46b316

--- src/mongo/db/storage/wiredtiger/wiredtiger_kv_engine.cpp
+++ src/mongo/db/storage/wiredtiger/wiredtiger_kv_engine.cpp
@@ -38,6 +38,7 @@
 #include "mongo/db/client.h"
 #include "mongo/db/commands/server_status/server_status_metric.h"
 #include "mongo/db/index_names.h"
+#include "mongo/db/replicated_fast_count/replicated_fast_count_manager.h"
 #include "mongo/db/rss/replicated_storage_service.h"
 #include "mongo/db/server_feature_flags_gen.h"
 #include "mongo/db/server_options.h"
@@ -1159,6 +1160,14 @@ void WiredTigerKVEngine::flushAllFiles(OperationContext* opCtx, bool callerHolds
 
     // Immediately flush the size storer information to disk. When the node is fsync locked for
     // operations such as backup, it's imperative that we copy the most up-to-date data files.
+    if (!_sizeStorer) {
+        if (gFeatureFlagReplicatedFastCount.isEnabledUseLastLTSFCVWhenUninitialized(
+                VersionContext::getDecoration(opCtx),
+                serverGlobalParams.featureCompatibility.acquireFCVSnapshot())) {
+            // TODO(SERVER-101413): Remove this once checkpointing and recovery is functional.
+            ReplicatedFastCountManager::get(opCtx->getServiceContext()).flush(opCtx);
+        }
+    }
     syncSizeInfo(true);
 
     // If there's no journal (ephemeral), we must checkpoint all of the data.
@@ -1575,8 +1584,9 @@ StatusWith<std::deque<std::string>> WiredTigerKVEngine::extendBackupCursor() {
 }
 
 void WiredTigerKVEngine::syncSizeInfo(bool sync) const {
-    if (!_sizeStorer)
+    if (!_sizeStorer) {
         return;
+    }
 
     while (true) {
         try {
